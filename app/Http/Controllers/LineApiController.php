76bc8657162fb4bac11b5d3e39f69796e39fdf59
Jamming0629 <ttajitaji1234taji@gmail.com>
2021-10-02 17:32:24 +0900

    fix20

--- app/Http/Controllers/LineApiController.php
+++ app/Http/Controllers/LineApiController.php
@@ -34,10 +34,10 @@ class LineApiController extends Controller
             // 返答に必要なトークンを取得
             $reply_token = $input['events'][0]['replyToken'];
             // テスト投稿の場合
-            if ($reply_token == '00000000000000000000000000000000') {
+            //if ($reply_token == '00000000000000000000000000000000') {
                 Log::info('Succeeded');
                 return;
-            }
+            //}
             // Lineに送信する準備
             $http_client = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($this->access_token);
             $bot         = new \LINE\LINEBot($http_client, ['channelSecret' => $this->channel_secret]);
@@ -84,7 +84,7 @@ class LineApiController extends Controller
             break;
     }
 
-    return $reply_token;
+    return;
     }
     
     // メッセージ送信用
