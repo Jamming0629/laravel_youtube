74beb96c04ae739cd2cdcbf63e2f9607403ac9eb
Jamming0629 <ttajitaji1234taji@gmail.com>
2021-10-04 02:41:44 +0900

    fixfix1

--- app/Http/Controllers/LineApiController.php
+++ app/Http/Controllers/LineApiController.php
@@ -23,91 +23,41 @@ class LineApiController extends Controller
     }
     // Webhook受取処理
     public function postWebhook(Request $request) {
-    //$input = $request->all();
-    // ユーザーがどういう操作を行った処理なのかを取得
-    //$type  = $input['events'][0]['type'];
-    $type = $request->input('events.0.type');
-    //dd($type);
-    //$type = $request['events'][0]['type'];
-    //dd($type);
-    // タイプごとに分岐
-    switch ($type) {
-        // メッセージ受信
-        case 'message':
-            // 返答に必要なトークンを取得
-            //$reply_token = $input['events'][0]['replyToken'];
-            $reply_token = $request->input('events.0.reply_token');
-            //$reply_token = $request['events'][0]['replyToken'];
-            // テスト投稿の場合
-            if ($reply_token == '00000000000000000000000000000000') {
-                Log::info('Succeeded');
-                return;
+ 
+        $http_client = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($this->access_token);
+        $bot         = new \LINE\LINEBot($http_client, ['channelSecret' => $this->channel_secret]);
+        
+        $signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
+        if(!signature){
+            return;
+        }
+        
+        $events = $bot->parseEventRequest($request->getContent(),$signature);
+        foreach($events as $event){
+            $lien_id = $event->getUserId();
+            $replyToken = $event->getReplyToken();
+            switch($event){
+                case($event instanceof FollowEvent):
+                    $message='友達登録ありがとう';
+                    $response = $bot->replyText($replyToken,$message);
+                    return;
+                
+                case($event instanceof UnfollowEvent):
+                    return;
+                    
+                case($event instanceof StickerMessage):
+                    $message = 'スタンプありがとう';
+                    $response = $bot->replyText($replyToken,$message);
+                    return;
+                    
+                case($event instanceof TextMessage):
+                    $send_Text = $event->getText();
+                    $message = $send_Text;
+                    $response = $bot->replyText($replyToken,$message);
+                    return;
             }
-            // Lineに送信する準備
-            $http_client = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($this->access_token);
-            $bot         = new \LINE\LINEBot($http_client, ['channelSecret' => $this->channel_secret]);
-            // LINEの投稿処理
-            $message_data = "メッセージありがとうございます。ただいま準備中です";
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message_data);
-            $response     = $bot->replyText($reply_token, $textMessageBuilder);
-            
-            //$http_client = new CurlHTTPClient(config('services.line.channel_token'));
-            //$bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
-            // 送信するメッセージの設定
-            //$reply_message='メッセージありがとうございます';
- 
-            // ユーザーにメッセージを返す
-            //$reply=$bot->replyText($reply_token, $reply_message);
-            
-            // Succeeded
-            if ($response->isSucceeded()) {
-                Log::info('返信成功');
-                break;
-            }
-            // Failed
-            Log::error($response->getRawBody());
-            break;
-            break;
-
-        // 友だち追加 or ブロック解除
-        case 'follow':
-            // ユーザー固有のIDを取得
-            $mid = $request['events'][0]['source']['userId'];
-            // ユーザー固有のIDはどこかに保存しておいてください。メッセージ送信の際に必要です。
-            LineUser::updateOrCreate(['line_id' => $mid]);
-            Log::info("ユーザーを追加しました。 user_id = " . $mid);
-            break;
-
-        // グループ・トークルーム参加
-        case 'join':
-            Log::info("グループ・トークルームに追加されました。");
-            break;
-
-        // グループ・トークルーム退出
-        case 'leave':
-            Log::info("グループ・トークルームから退出させられました。");
-            break;
-
-        // ブロック
-        case 'unfollow':
-            Log::info("ユーザーにブロックされました。");
-            break;
-
-        default:
-            //$reply_token = $request->input('events.0.replyToken');
-            /*$reply_token = $request['events'][0]['replyToken'];
-            $http_client = new CurlHTTPClient(config('services.line.channel_token'));
-            $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
- 
-            // 送信するメッセージの設定
-            $reply_message='メッセージありがとうございます';
- 
-            // ユーザーにメッセージを返す
-            $reply=$bot->replyText($reply_token, $reply_message);*/
-            Log::info("the type is" . $type);
-            break;
-    }
-    //return;
+        }
+        
     }
     
     // メッセージ送信用
