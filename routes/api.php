08ed65461779d4f982fde80c4a006ef4d3f47594
Jamming0629 <ttajitaji1234taji@gmail.com>
2021-10-04 03:17:38 +0900

    fixfix5

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::group(['namespace' => 'Api'], function() {
     // LineからのWebhookを受信
-    Route::post('/line/webhook', 'App\Http\Controllers\LineApiController@webhook')->name('line.webhook');
+    Route::post('/line/webhook', 'App\Http\Controllers\api\LineApiController@webhook')->name('line.webhook');
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
