c686ab1fd7da633678d7dd5994a46ecf2e818af2
Jamming0629 <86952073+Jamming0629@users.noreply.github.com>
2023-04-10 17:03:24 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
     return 'welcome';
 });*/
 
-//Route::get('/index', 'App\Http\Controllers\YouTubeController@index')->name('index');
+Route::get('/index', 'App\Http\Controllers\YouTubeController@index')->name('index');
 Route::get('/', 'App\Http\Controllers\YouTubeController@index')->name('index');
-Route::get('/results', 'App\Http\Controllers\YouTubeController@results')->name('results');
+//Route::get('/results', 'App\Http\Controllers\YouTubeController@results')->name('results');
 Route::get('/watch/{id}', 'App\Http\Controllers\YouTubeController@watch')->name('watch');
 //Route::get('/', 'App\Http\Controllers\Auth\LoginController@redirectToGoogle');
 
