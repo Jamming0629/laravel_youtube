5d818f65a11fb85ed6e713fc5ac5ed22587a2cb8
Jamming0629 <86952073+Jamming0629@users.noreply.github.com>
2023-04-10 18:21:42 +0900

    Update 2021_09_04_081144_create_sessions_table.php

--- database/migrations/2021_09_04_081144_create_sessions_table.php
+++ database/migrations/2021_09_04_081144_create_sessions_table.php
@@ -13,14 +13,14 @@ class CreateSessionsTable extends Migration
      */
     public function up()
     {
-        /*Schema::create('sessions', function (Blueprint $table) {
+        Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->text('payload');
             $table->integer('last_activity')->index();
-        });*/
+        });
     }
 
     /**
@@ -30,6 +30,6 @@ class CreateSessionsTable extends Migration
      */
     public function down()
     {
-        //Schema::dropIfExists('sessions');
+        Schema::dropIfExists('sessions');
     }
 }
